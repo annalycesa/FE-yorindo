d78c9fd88a4d7b17492b113e84cb2fc0fcb5fef9
alyceinwonderland <annalyce75@gmail.com>
2026-03-26 15:27:34 +0700

    initial add router

---
+++ src/pages/Event.tsx
@@ -0,0 +1,5 @@
+const Event = () => {
+    return <h1>Halaman Event List</h1>;
+}
+
+export default Event;
---
+++ src/router/index.tsx
@@ -0,0 +1,23 @@
+// import { createBrowserRouter } from 'react-router-dom';
+// import Home from '../pages/Home';
+// import About from '../pages/About';
+// import Layout from '../components/Layout'; // Opsional: Untuk Navbar permanen
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />, // Jika ingin ada Navbar/Footer di setiap halaman
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;
